5bcc9df2b8ae0f0a673df1f30518142efbc7b5c7
Ami Hays <amihays@Amis-MacBook-Pro.local>
2015-09-22 17:55:41 -0700

    finished phases 1-5

---
+++ lib/phase5/params.rb
@@ -0,0 +1,60 @@
+require 'uri'
+require 'byebug'
+
+module Phase5
+  class Params
+    # use your initialize to merge params from
+    # 1. query string
+    # 2. post body
+    # 3. route params
+    #
+    # You haven't done routing yet; but assume route params will be
+    # passed in as a hash to `Params.new` as below:
+    def initialize(req, route_params = {})
+      @params = route_params
+      parse_www_encoded_form(req.query_string)
+      parse_www_encoded_form(req.body)
+    end
+
+    def [](key)
+      unless key.is_a?(Symbol) || key.is_a?(String)
+        raise "Not a symbol or string"
+      end
+      @params[key.to_s] || @params[key.to_sym]
+    end
+
+    # this will be useful if we want to `puts params` in the server log
+    def to_s
+      @params.to_s
+    end
+
+    class AttributeNotFoundError < ArgumentError; end;
+
+    private
+    # this should return deeply nested hash
+    # argument format
+    # user[address][street]=main&user[address][zip]=89436
+    # should return
+    # { "user" => { "address" => { "street" => "main", "zip" => "89436" } } }
+    def parse_www_encoded_form(www_encoded_form)
+      return nil unless www_encoded_form
+      arr = URI::decode_www_form(www_encoded_form)
+
+      arr.each do |pair| # @params[pair[0]] = pair[1]
+        keys_array = parse_key(pair[0])
+        current = @params
+        keys_array[0...-1].each do |key|
+          current[key] ||= {}
+          current = current[key]
+        end
+        current[keys_array.last] = pair[1]
+      end
+    end
+
+    # this should return an array
+    # user[address][street] should return ['user', 'address', 'street']
+    def parse_key(key)
+      key.split(/\]\[|\[|\]/)
+    end
+  end
+end
